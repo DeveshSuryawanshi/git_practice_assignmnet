11b7e73bd8e4b4a2b1f8722c28bf0f9c9bb1a9bb
Devesh.Suryawanshi <deveshsuryawashi940@gmail.com>
2022-11-29 21:31:01 +0530

    pushing files

---
+++ palindrome.js
@@ -0,0 +1,9 @@
+let str = "naman";
+for(let i=0; i<str.length; i++){
+    bag += str[i];
+}
+if(bag == str){
+    console.log("Yes");
+}else{
+    console.log("Yes"); //////////////////
+}
---
+++ prime.js
@@ -0,0 +1,12 @@
+let num = 5;
+
+for(let i=1; i<num; i++){
+    if(num %i == 1){
+        count++;
+    }
+}
+if(count == 2){
+    console.log("No");/////////
+}else{
+    console.log("Yes");////////
+}
